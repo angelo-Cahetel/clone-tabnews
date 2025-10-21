b8b0e0b9640d1f6767e555b847fa49462dcce3a5
Angelo Mendes <angelo030825@gmail.com>
2025-10-21 11:52:37 -0300

    adiciona database.js e .env

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
-  response.status(200).json({ status: 'eu sou aluno do curso.dev e sou acima da média' })
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response
+    .status(200)
+    .json({ status: "eu sou aluno do curso.dev e sou acima da média" });
 }
 
-export default status
+export default status;
