a7a96705713b62738e81558395e4ceac8d6b6ddb
Angelo Mendes <angelo030825@gmail.com>
2025-10-22 11:16:45 -0300

    endpoint status: ISO 8601 d20

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,11 +1,11 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response
-    .status(200)
-    .json({ status: "eu sou aluno do curso.dev e sou acima da média" });
+  const updateAt = new Date().toISOString();
+
+  response.status(200).json({ 
+    update_at: updateAt, 
+  });
 }
 
 export default status;
