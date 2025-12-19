9edbbe71a5aeeead7a25de92eeb371ff119d32f2
Angelo Mendes <angelo030825@gmail.com>
2025-12-19 15:30:36 -0300

    feat: send activation email after `user` registration

---
+++ infra/webserver.js
@@ -0,0 +1,17 @@
+function getOrigin() {
+  if (["tests", "development"].includes(process.env.NODE_ENV)) {
+    return "http://localhost:3000";
+  }
+
+  if (process.env.VERCEL_ENV === "preview") {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  return "https://clone-tabnews-ten-psi-77.vercel.app/";
+}
+
+const webserver = {
+  origin: getOrigin(),
+};
+
+export default webserver;
---
+++ tests/integration/_use-case/registration-flow.test.js
@@ -0,0 +1,62 @@
+import orchestrator from "tests/orchestrator";
+import activation from "models/activation.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+  await orchestrator.deleteAllEmails();
+});
+
+describe("Use case: Registration Flow (all successful)", () => {
+  let createUserResponseBody;
+
+  test("Create user account", async () => {
+    const createUserResponse = await fetch(
+      "http://localhost:3000/api/v1/users",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "RegistrationFlow",
+          email: "registration.flow@example.com",
+          password: "RegistrationFlowPassword",
+        }),
+      },
+    );
+
+    expect(createUserResponse.status).toBe(201);
+
+    createUserResponseBody = await createUserResponse.json();
+
+    expect(createUserResponseBody).toEqual({
+      id: createUserResponseBody.id,
+      username: "RegistrationFlow",
+      email: "registration.flow@example.com",
+      password: createUserResponseBody.password,
+      features: ["read:activation_token"],
+      created_at: createUserResponseBody.created_at,
+      updated_at: createUserResponseBody.updated_at,
+    });
+  });
+
+  test("Recive activation email", async () => {
+    const lastEmail = await orchestrator.getLastEmail();
+
+    const activationToken = await activation.findOneByUserId(
+      createUserResponseBody.id,
+    );
+
+    expect(lastEmail.sender).toBe("<contato@angelodev.com.br>");
+    expect(lastEmail.recipients[0]).toBe("<registration.flow@example.com>");
+    expect(lastEmail.subject).toBe("Ative seu cadastro no TabNews!");
+    expect(lastEmail.text).toContain("RegistrationFlow");
+    expect(lastEmail.text).toContain(activationToken.id);
+  });
+
+  test("Activate account", async () => {});
+
+  test("Login", async () => {});
+});
